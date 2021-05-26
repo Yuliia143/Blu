41235ed12ad4fb97ad505bdb19d4837f9cb70ff5
Yulya Humenyuk <yhumenyuk@reenbit.com>
2021-05-26 17:47:19 +0300

    Do scan, connect and read

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController, ToastController } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 import { BLE } from '@ionic-native/ble/ngx';
@@ -16,6 +16,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     HomePageRoutingModule
   ],
   declarations: [HomePage],
-  providers: [BLE]
+  providers: [BLE, NavController, ToastController]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { BLE } from '@ionic-native/ble/ngx';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +8,103 @@ import { BLE } from '@ionic-native/ble/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public devices = [];
+  devices: any[] = [];
+  peripheral: any = {};
+  statusMessage: string;
+  data: any;
+  data1: any;
+  data2: any;
+  data3: any;
+  data4: any;
 
-  constructor(private ble: BLE) {}
+  constructor(public navCtrl: NavController,
+              private toastCtrl: ToastController,
+              private ble: BLE,
+              private ngZone: NgZone) {
+  }
 
-  public scan(): void {
-    this.ble.scan([], 30).subscribe(device => {
-      window.alert(JSON.stringify(device));
-      console.log(device);
+  scan() {
+    this.setStatus('Scanning');
+    this.devices = [];
+
+    this.ble.scan([], 5).subscribe(
+      device => this.onDeviceDiscovered(device),
+      error => this.scanError(error)
+    );
+
+    setTimeout(this.setStatus.bind(this), 5000, 'Scan complete');
+  }
+
+  onDeviceDiscovered(device) {
+    console.log('Discovered ' + JSON.stringify(device, null, 2));
+    this.ngZone.run(() => {
       this.devices.push(device);
     });
   }
+
+  async scanError(error) {
+    this.setStatus('Error ' + error);
+    const toast = await this.toastCtrl.create({
+      message: 'Error scanning for Bluetooth low energy devices',
+      position: 'middle',
+      duration: 5000
+    });
+    toast.present();
+  }
+
+  setStatus(message) {
+    console.log(message);
+    this.ngZone.run(() => {
+      this.statusMessage = message;
+    });
+  }
+
+  deviceSelected(device) {
+    this.setStatus('Connecting to ' + device.name || device.id);
+    this.ble.connect(device.id).subscribe(
+      peripheral => this.onConnected(peripheral),
+      peripheral => this.onDeviceDisconnected(peripheral)
+    );
+  }
+
+  onConnected(peripheral) {
+    this.ngZone.run(() => {
+      this.setStatus('Connected');
+      this.peripheral = peripheral;
+        this.ble.read(this.peripheral.id,'1800', '2a00').then(
+          data => this.data1 = data,
+          error => this.setStatus('Error ' + error)
+        );
+      this.ble.read(this.peripheral.id,'1800', '2a01').then(
+        data => this.data2 = data,
+        error => this.setStatus('Error ' + error)
+      );
+      this.ble.read(this.peripheral.id,'180a', '2a25').then(
+        data => this.data3 = data,
+        error => this.setStatus('Error ' + error)
+      );
+      this.ble.read(this.peripheral.id,'fee1', 'fed0').then(
+        data => this.data4 = data,
+        error => this.setStatus('Error ' + error)
+      );
+
+      // this.peripheral.characteristics.forEach(characteristic => {
+      //   this.ble.read(this.peripheral.id, characteristic.service, characteristic.characteristic).then(
+      //     data => this.data.push(data),
+      //     error => this.setStatus('Error ' + error)
+      //   );
+      // });
+    });
+  }
+
+  async onDeviceDisconnected(peripheral) {
+    const toast = await this.toastCtrl.create({
+      message: 'The peripheral unexpectedly disconnected',
+      duration: 3000,
+      position: 'middle'
+    });
+    toast.present();
+  }
+
 
 }
