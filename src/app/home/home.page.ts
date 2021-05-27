0a7770b47a964a869e1f81043b0636128e01bdc3
Yulya Humenyuk <yhumenyuk@reenbit.com>
2021-05-27 17:57:26 +0300

    Add diff formats

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,6 +17,19 @@ export class HomePage {
   data3: any;
   data4: any;
 
+  data5: any;
+  data5JSON: any;
+  data5float: any;
+  data5St: any;
+  data5ISt: any;
+  data5L: any;
+  data5A: any;
+  data5StFromChar: any;
+  data5A0: any;
+  data5A4: any;
+  data5A002: any;
+  data5A001: any;
+
   constructor(public navCtrl: NavController,
               private toastCtrl: ToastController,
               private ble: BLE,
@@ -71,29 +84,39 @@ export class HomePage {
     this.ngZone.run(() => {
       this.setStatus('Connected');
       this.peripheral = peripheral;
-        this.ble.read(this.peripheral.id,'1800', '2a00').then(
-          data => this.data1 = data,
-          error => this.setStatus('Error ' + error)
-        );
-      this.ble.read(this.peripheral.id,'1800', '2a01').then(
+      this.ble.read(this.peripheral.id, '1800', '2a00').then(
+        data => {
+          this.data1 = data;
+        },
+        error => this.setStatus('Error ' + error)
+      );
+      this.ble.read(this.peripheral.id, '1800', '2a01').then(
         data => this.data2 = data,
         error => this.setStatus('Error ' + error)
       );
-      this.ble.read(this.peripheral.id,'180a', '2a25').then(
+      this.ble.read(this.peripheral.id, '180a', '2a25').then(
         data => this.data3 = data,
         error => this.setStatus('Error ' + error)
       );
-      this.ble.read(this.peripheral.id,'fee1', 'fed0').then(
-        data => this.data4 = data,
+
+      this.ble.read(this.peripheral.id, 'fee0', '00000006-0000-3512-2118-0009af100700').then( //battery
+        data => {
+          this.data5 = data;
+
+          this.data5JSON = JSON.stringify(data);
+          this.data5float = new Float32Array(data);
+          this.data5St = String.fromCharCode.apply(null,new Uint8Array(data));
+          this.data5ISt = new Int8Array(data).toString();
+          this.data5L = data.byteLength;
+          this.data5A = new Uint8Array(data);
+          this.data5StFromChar = this.data5A.map(elem => String.fromCharCode(elem));
+          this.data5A002 = new Uint8Array(data)[0].toString(2);
+          this.data5A001 = new Uint8Array(data)[0].toString(10);
+          this.data5A0 = new DataView(data).getInt32(0);
+        },
         error => this.setStatus('Error ' + error)
       );
 
-      // this.peripheral.characteristics.forEach(characteristic => {
-      //   this.ble.read(this.peripheral.id, characteristic.service, characteristic.characteristic).then(
-      //     data => this.data.push(data),
-      //     error => this.setStatus('Error ' + error)
-      //   );
-      // });
     });
   }
 
